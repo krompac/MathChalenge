8205ce11ec33da9ce8e77bd9bdbb50e26b52f850
krompac <tomijehrvat@gmail.com>
2020-09-27 12:27:36 +0200

    Added buttons and input.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('textInput') textInput: ElementRef;
+
+  clear = 'Clear';
+  check = 'Check';
+
+  data = [
+    [1, 4, 7, this.clear],
+    [2, 5, 8, 0],
+    [3, 6, 9, this.check],
+  ];
 
   constructor() {}
 
+  onClick(event: any) {
+    console.log(event.target.innerText);
+  }
+
+  dataEntry(rowNumber: number, columnNumber: number) {
+    return this.data[rowNumber][columnNumber];
+  }
 }
